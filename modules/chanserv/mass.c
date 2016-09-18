015999cac33b3500e444bb154617561605357126
Austin Ellis <siniStar@IRC4Fun.net>
2016-09-18 16:31:49 -0500

    chanserv/mass: halfops,op,owner,protect,voice
    Mass functions must be enabled, by default they are not.  (Closes #11)

---
+++ modules/chanserv/mass.c
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2014-2016 Xtheme Development Group (Xtheme.org)
+ * Rights to this code are as documented in doc/LICENSE.
+ *
+ * This file contains code for the CService MASS functions.
+ *
+ */
+
+#include "atheme.h"
+
+DECLARE_MODULE_V1
+(
+	"chanserv/mass", false, _modinit, _moddeinit,
+	PACKAGE_STRING,
+	VENDOR_STRING
+);
+
+static void cs_cmd_mass(sourceinfo_t *si, int parc, char *parv[]);
+static void cs_help_mass(sourceinfo_t *si, const char *subcmd);
+
+command_t cs_mass = { "MASS", N_("Channel removal toolkit."),
+                        AC_NONE, 3, cs_cmd_mass, { .func = cs_help_mass } };
+
+mowgli_patricia_t *cs_mass_cmds;
+
+void _modinit(module_t *m)
+{
+        service_named_bind_command("chanserv", &cs_mass);
+
+	cs_mass_cmds = mowgli_patricia_create(strcasecanon);
+}
+
+void _moddeinit(module_unload_intent_t intent)
+{
+	service_named_unbind_command("chanserv", &cs_mass);
+
+	mowgli_patricia_destroy(cs_mass_cmds, NULL, NULL);
+}
+
+static void cs_help_mass(sourceinfo_t *si, const char *subcmd)
+{
+	if (!subcmd)
+	{
+		command_success_nodata(si, _("***** \2%s Help\2 *****"), chansvs.me->disp);
+		command_success_nodata(si, _("Help for \2MASS\2:"));
+		command_success_nodata(si, " ");
+		command_success_nodata(si, _("MASS allows you to perform MASS functions on a channel."));
+		command_success_nodata(si, " ");
+		command_help(si, cs_mass_cmds);
+		command_success_nodata(si, " ");
+		command_success_nodata(si, _("For more information, use \2/msg %s HELP MASS \37command\37\2."), chansvs.me->disp);
+		command_success_nodata(si, _("***** \2End of Help\2 *****"));
+	}
+	else
+		help_display(si, si->service, subcmd, cs_mass_cmds);
+}
+
+static void cs_cmd_mass(sourceinfo_t *si, int parc, char *parv[])
+{
+	char *chan;
+	char *cmd;
+	command_t *c;
+
+	if (parc < 2)
+	{
+		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "MASS");
+		command_fail(si, fault_needmoreparams, _("Syntax: MASS <#channel> <command> [parameters]"));
+		return;
+	}
+
+	if (parv[0][0] == '#')
+		chan = parv[0], cmd = parv[1];
+	else if (parv[1][0] == '#')
+		cmd = parv[0], chan = parv[1];
+	else
+	{
+		command_fail(si, fault_badparams, STR_INVALID_PARAMS, "MASS");
+		command_fail(si, fault_badparams, _("Syntax: MASS <#channel> <command> [parameters]"));
+		return;
+	}
+
+	c = command_find(cs_mass_cmds, cmd);
+	if (c == NULL)
+	{
+		command_fail(si, fault_badparams, _("Invalid command. Use \2/%s%s help\2 for a command listing."), (ircd->uses_rcommand == false) ? "msg " : "", chansvs.me->disp);
+		return;
+	}
+
+	parv[1] = chan;
+	command_exec(si->service, si, c, parc - 1, parv + 1);
+}
+
+
---
+++ modules/chanserv/mass_protect.c
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2014-2016 Xtheme Development Group (Xtheme.org)
+ * Rights to this code are as documented in doc/LICENSE.
+ *
+ * This file contains code for the ChanServ MASS PROTECT function.
+ *
+ */
+
+#include "atheme.h"
+
+DECLARE_MODULE_V1
+(
+	"chanserv/mass_protect", false, _modinit, _moddeinit,
+	PACKAGE_STRING,
+	VENDOR_STRING
+);
+
+static void cs_cmd_mass_protect(sourceinfo_t *si, int parc, char *parv[]);
+
+command_t cs_mass_protect = { "PROTECT", N_("Protects all channel users on a channel."),
+	AC_NONE, 2, cs_cmd_mass_protect, { .path = "cservice/mass_protect" } };
+
+mowgli_patricia_t **cs_mass_cmds;
+
+void _modinit(module_t *m)
+{
+	if (ircd != NULL && !ircd->uses_protect)
+	{
+		slog(LG_INFO, "Module %s requires protect support, refusing to load.", m->name);
+		m->mflags = MODTYPE_FAIL;
+		return;
+	}
+
+	MODULE_TRY_REQUEST_SYMBOL(m, cs_mass_cmds, "chanserv/mass", "cs_mass_cmds");
+
+	command_add(&cs_mass_protect, *cs_mass_cmds);
+}
+
+void _moddeinit(module_unload_intent_t intent)
+{
+	command_delete(&cs_mass_protect, *cs_mass_cmds);
+}
+
+static void cs_cmd_mass_protect(sourceinfo_t *si, int parc, char *parv[])
+{
+	channel_t *c;
+	char *channel = parv[0];
+	mychan_t *mc = mychan_find(channel);
+	chanuser_t *cu, *origin_cu = NULL;
+	mowgli_node_t *n, *tn;
+
+	if (!mc)
+	{
+		command_fail(si, fault_nosuch_target, _("Channel \2%s\2 is not registered."), channel);
+		return;
+	}
+	
+	if (metadata_find(mc, "private:frozen:freezer"))
+	{
+		command_fail(si, fault_noprivs, _("\2%s\2 is frozen."), channel);
+		return;
+	}
+
+	if (!(c = channel_find(channel)))
+	{
+		command_fail(si, fault_nosuch_target, _("\2%s\2 is currently empty."), channel);
+		return;
+	}
+
+	if (!chanacs_source_has_flag(mc, si, CA_RECOVER))
+	{
+		command_fail(si, fault_noprivs, _("You are not authorized to perform this operation."));
+		return;
+	}
+
+	if (!chanacs_source_has_flag(mc, si, CA_USEPROTECT))
+	{
+		command_fail(si, fault_noprivs, _("You are not authorized to perform this operation."));
+		return;
+	}
+
+	if (chanacs_source_has_flag(mc, si, CA_SUSPENDED))
+	{
+		command_fail(si, fault_noprivs, _("Your access in %s is \2suspended\2."), channel);
+		return;
+	}
+
+	if (metadata_find(mc, "private:close:closer"))
+	{
+		command_fail(si, fault_noprivs, _("\2%s\2 is closed."), channel);
+		return;
+	}
+	
+	/* protect everyone */
+			MOWGLI_ITER_FOREACH(n, mc->chan->members.head)
+			{
+					cu = (chanuser_t *)n->data;
+					if (is_internal_client(cu->user))
+							;
+					else
+					{
+						modestack_mode_param(chansvs.nick, mc->chan, MTYPE_ADD, ircd->protect_mchar[1], CLIENT_NAME(cu->user));
+						cu->modes &= ~ircd->protect_mode;
+					}
+			}
+
+	logcommand(si, CMDLOG_DO, "MASS:PROTECT: \2%s\2", mc->name);
+
+	command_success_nodata(si, _("Mass Protected channel users in \2%s\2."), channel);
+}
+
+
