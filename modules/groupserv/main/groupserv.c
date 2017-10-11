8ebe5a0aa4b4b6d6df09c571a339d0fe5a5c08ea
Austin Ellis <siniStar@IRC4Fun.net>
2017-10-11 08:56:17 -0500

    LG_VERBOSE updates (missed one)

--- modules/groupserv/main/groupserv.c
+++ modules/groupserv/main/groupserv.c
@@ -354,7 +354,7 @@ void remove_group_chanacs(mygroup_t *mg)
 		if (ca->level & CA_FOUNDER && mychan_num_founders(mc) == 1 && (successor = mychan_pick_successor(mc)) != NULL)
 		{
 			slog(LG_INFO, _("SUCCESSION: \2%s\2 to \2%s\2 from \2%s\2"), mc->name, entity(successor)->name, entity(mg)->name);
-			slog(LG_VERBOSE, "myuser_delete(): giving channel %s to %s (unused %lds, founder %s, chanacs %zu)",
+			slog(LG_VERBOSE, "USER:DELETE: giving channel %s to %s (unused %lds, founder %s, chanacs %zu)",
 					mc->name, entity(successor)->name,
 					(long)(CURRTIME - mc->used),
 					entity(mg)->name,
@@ -371,7 +371,7 @@ void remove_group_chanacs(mygroup_t *mg)
 		else if (ca->level & CA_FOUNDER && mychan_num_founders(mc) == 1)
 		{
 			slog(LG_REGISTER, _("DELETE: \2%s\2 from \2%s\2"), mc->name, entity(mg)->name);
-			slog(LG_VERBOSE, "myuser_delete(): deleting channel %s (unused %lds, founder %s, chanacs %zu)",
+			slog(LG_VERBOSE, "USER:DELETE: deleting channel %s (unused %lds, founder %s, chanacs %zu)",
 					mc->name, (long)(CURRTIME - mc->used),
 					entity(mg)->name,
 					MOWGLI_LIST_LENGTH(&mc->chanacs));
