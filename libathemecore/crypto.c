52ea65d4ec9e9a60a3c2bfb393b58ca245ed2931
Austin Ellis <siniStar@IRC4Fun.net>
2017-12-07 21:21:42 -0600

    Revert "libathemecore/crypto.c: crypt_string(): generate salt if not given one"
    This reverts commit df29296c7bc1d838b8a1f90ba864dcefafdd7531.

--- libathemecore/crypto.c
+++ libathemecore/crypto.c
@@ -76,16 +76,8 @@ static void
 crypt_log_modchg(const char *const restrict caller, const char *const restrict which,
                  const crypt_impl_t *const restrict impl)
 {
-	const crypt_impl_t *ci = crypt_get_default_provider();
-
-	if (!salt || !*salt)
-		salt = ci->salt();
-
-	if (!salt)
-		return NULL;
-
-	return ci->crypt(key, salt);
-}
+	const unsigned int level = (runflags & RF_STARTING) ? LG_DEBUG : LG_INFO;
+	const crypt_impl_t *const ci = crypt_get_default_provider();
 
 	(void) slog(level, "%s: %s crypto provider '%s'", caller, which, impl->id);
 
