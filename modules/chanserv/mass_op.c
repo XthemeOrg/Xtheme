15f78a27656aefa2b31c864334c817996c3e8a34
Austin Ellis <siniStar@IRC4Fun.net>
2018-03-23 10:30:29 -0500

    chanserv/mass: add de-commands
    Add ChanServ MASS DECOMMANDS to remove statuses on a whole channel.

--- modules/chanserv/mass_op.c
+++ modules/chanserv/mass_op.c
@@ -16,9 +16,12 @@ DECLARE_MODULE_V1
 );
 
 static void cs_cmd_mass_op(sourceinfo_t *si, int parc, char *parv[]);
+static void cs_cmd_mass_deop(sourceinfo_t *si, int parc, char *parv[]);
 
 command_t cs_mass_op = { "OP", N_("Ops all users on a channel."),
 	AC_NONE, 2, cs_cmd_mass_op, { .path = "cservice/mass_op" } };
+command_t cs_mass_deop = { "DEOP", N_("Deops all users on a channel."),
+	AC_NONE, 2, cs_cmd_mass_deop, { .path = "cservice/mass_deop" } };
 
 mowgli_patricia_t **cs_mass_cmds;
 
@@ -27,11 +30,13 @@ void _modinit(module_t *m)
 	MODULE_TRY_REQUEST_SYMBOL(m, cs_mass_cmds, "chanserv/mass", "cs_mass_cmds");
 
 	command_add(&cs_mass_op, *cs_mass_cmds);
+	command_add(&cs_mass_deop, *cs_mass_cmds);
 }
 
 void _moddeinit(module_unload_intent_t intent)
 {
 	command_delete(&cs_mass_op, *cs_mass_cmds);
+	command_delete(&cs_mass_deop, *cs_mass_cmds);
 }
 
 static void cs_cmd_mass_op(sourceinfo_t *si, int parc, char *parv[])
@@ -109,8 +114,77 @@ static void cs_cmd_mass_op(sourceinfo_t *si, int parc, char *parv[])
 	command_success_nodata(si, _("Mass Opped channel users in \2%s\2."), channel);
 }
 
-/* vim:cinoptions=>s,e0,n0,f0,{0,}0,^0,=s,ps,t0,c3,+s,(2s,us,)20,*30,gs,hs
- * vim:ts=8
- * vim:sw=8
- * vim:noexpandtab
- */
+static void cs_cmd_mass_deop(sourceinfo_t *si, int parc, char *parv[])
+{
+	channel_t *c;
+	char *channel = parv[0];
+	mychan_t *mc = mychan_find(channel);
+	chanuser_t *cu, *origin_cu = NULL;
+	mowgli_node_t *n, *tn;
+
+	if (!mc)
+	{
+		command_fail(si, fault_nosuch_target, _("Channel \2%s\2 is not registered."), channel);
+		return;
+	}
+
+	if (metadata_find(mc, "private:frozen:freezer"))
+	{
+		command_fail(si, fault_noprivs, _("\2%s\2 is frozen."), channel);
+		return;
+	}
+
+	if (!(c = channel_find(channel)))
+	{
+		command_fail(si, fault_nosuch_target, _("\2%s\2 is currently empty."), channel);
+		return;
+	}
+
+	if (!chanacs_source_has_flag(mc, si, CA_OP))
+	{
+		command_fail(si, fault_noprivs, _("You are not authorized to perform this operation."));
+		return;
+	}
+
+	if (mc->flags & MC_SECURE)
+	{
+		command_fail(si, fault_noprivs, _("%s has SECURE enabled."), channel);
+		return;
+	}
+
+	if (!chanacs_source_has_flag(mc, si, CA_RECOVER))
+	{
+		command_fail(si, fault_noprivs, _("You are not authorized to perform this operation."));
+		return;
+	}
+
+
+	if (chanacs_source_has_flag(mc, si, CA_SUSPENDED))
+	{
+		command_fail(si, fault_noprivs, _("Your access in %s is \2suspended\2."), channel);
+		return;
+	}
+
+	if (metadata_find(mc, "private:close:closer"))
+	{
+		command_fail(si, fault_noprivs, _("\2%s\2 is closed."), channel);
+		return;
+	}
+
+	/* de-op everyone */
+			MOWGLI_ITER_FOREACH(n, mc->chan->members.head)
+			{
+					cu = (chanuser_t *)n->data;
+					if (is_internal_client(cu->user))
+							;
+					else
+					{
+						modestack_mode_param(chansvs.nick, mc->chan, MTYPE_DEL, 'o', CLIENT_NAME(cu->user));
+						cu->modes &= ~CSTATUS_OP;
+					}
+			}
+
+	logcommand(si, CMDLOG_DO, "MASS:DEOP: \2%s\2", mc->name);
+
+	command_success_nodata(si, _("Mass Deopped channel users in \2%s\2."), channel);
+}
--- modules/chanserv/mass_voice.c
+++ modules/chanserv/mass_voice.c
@@ -16,9 +16,12 @@ DECLARE_MODULE_V1
 );
 
 static void cs_cmd_mass_voice(sourceinfo_t *si, int parc, char *parv[]);
+static void cs_cmd_mass_devoice(sourceinfo_t *si, int parc, char *parv[]);
 
 command_t cs_mass_voice = { "VOICE", N_("Voices all users on a channel."),
 	AC_NONE, 2, cs_cmd_mass_voice, { .path = "cservice/mass_voice" } };
+command_t cs_mass_devoice = { "DEVOICE", N_("De-voices all users on a channel."),
+	AC_NONE, 2, cs_cmd_mass_devoice, { .path = "cservice/mass_devoice" } };
 
 mowgli_patricia_t **cs_mass_cmds;
 
@@ -27,11 +30,13 @@ void _modinit(module_t *m)
 	MODULE_TRY_REQUEST_SYMBOL(m, cs_mass_cmds, "chanserv/mass", "cs_mass_cmds");
 
 	command_add(&cs_mass_voice, *cs_mass_cmds);
+	command_add(&cs_mass_devoice, *cs_mass_cmds);
 }
 
 void _moddeinit(module_unload_intent_t intent)
 {
 	command_delete(&cs_mass_voice, *cs_mass_cmds);
+	command_delete(&cs_mass_devoice, *cs_mass_cmds);
 }
 
 static void cs_cmd_mass_voice(sourceinfo_t *si, int parc, char *parv[])
@@ -100,4 +105,72 @@ static void cs_cmd_mass_voice(sourceinfo_t *si, int parc, char *parv[])
 	logcommand(si, CMDLOG_DO, "MASS:VOICE: \2%s\2", mc->name);
 
 	command_success_nodata(si, _("Mass Voiced channel users in \2%s\2."), channel);
+}
+
+static void cs_cmd_mass_devoice(sourceinfo_t *si, int parc, char *parv[])
+{
+	channel_t *c;
+	char *channel = parv[0];
+	mychan_t *mc = mychan_find(channel);
+	chanuser_t *cu, *origin_cu = NULL;
+	mowgli_node_t *n, *tn;
+
+	if (!mc)
+	{
+		command_fail(si, fault_nosuch_target, _("Channel \2%s\2 is not registered."), channel);
+		return;
+	}
+
+	if (metadata_find(mc, "private:frozen:freezer"))
+	{
+		command_fail(si, fault_noprivs, _("\2%s\2 is frozen."), channel);
+		return;
+	}
+
+	if (!(c = channel_find(channel)))
+	{
+		command_fail(si, fault_nosuch_target, _("\2%s\2 is currently empty."), channel);
+		return;
+	}
+
+	if (!chanacs_source_has_flag(mc, si, CA_RECOVER))
+	{
+		command_fail(si, fault_noprivs, _("You are not authorized to perform this operation."));
+		return;
+	}
+
+	if (!chanacs_source_has_flag(mc, si, CA_VOICE))
+	{
+		command_fail(si, fault_noprivs, _("You are not authorized to perform this operation."));
+		return;
+	}
+
+	if (chanacs_source_has_flag(mc, si, CA_SUSPENDED))
+	{
+		command_fail(si, fault_noprivs, _("Your access in %s is \2suspended\2."), channel);
+		return;
+	}
+
+	if (metadata_find(mc, "private:close:closer"))
+	{
+		command_fail(si, fault_noprivs, _("\2%s\2 is closed."), channel);
+		return;
+	}
+
+	/* de-voice everyone */
+			MOWGLI_ITER_FOREACH(n, mc->chan->members.head)
+			{
+					cu = (chanuser_t *)n->data;
+					if (is_internal_client(cu->user))
+							;
+					else
+					{
+							modestack_mode_param(chansvs.nick, mc->chan, MTYPE_DEL, 'v', CLIENT_NAME(cu->user));
+							cu->modes &= ~CSTATUS_VOICE;
+					}
+			}
+
+	logcommand(si, CMDLOG_DO, "MASS:DEVOICE: \2%s\2", mc->name);
+
+	command_success_nodata(si, _("Mass De-Voiced channel users in \2%s\2."), channel);
 }
